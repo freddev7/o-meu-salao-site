7a6f06dc9b871bc956b50539352d43bfb81017fd
Frederico <fredfrgs@gmail.com>
2022-10-09 01:49:05 -0300

    media queries prod

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Services from './pages/Services/Services'
