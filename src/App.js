d312cf42c690967c4d3728565cb52e93e7d2c350
Frederico <fredfrgs@gmail.com>
2023-04-09 18:37:50 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
 import Services from './pages/Services/Services'
@@ -13,20 +13,20 @@ import Blog from './pages/Blog/Blog';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Services" element={<Services />} />
-        <Route path="/Products" element={<Products />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Invisible" element={<Invisible />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Courses" element={<Courses />} />
-        <Route path="/Franchise" element={<Franchise />} />
-        <Route path="/Blog" element={<Blog />} />
-      </Routes>
-    </>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Invisible" element={<Invisible />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Courses" element={<Courses />} />
+          <Route path="/Franchise" element={<Franchise />} />
+          <Route path="/Blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
