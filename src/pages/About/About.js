1233160b94057f98280d3b726ac677fcc0745669
Frederico <fredfrgs@gmail.com>
2023-08-03 21:23:10 -0300

    updates about and services

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -11,41 +11,40 @@ import carouselimgthree from '../../assets/img/carouselimgthree.jpg'
 import carouselimgfour from '../../assets/img/carouselimgfour.jpg'
 import carouselimgfive from '../../assets/img/carouselimgfive.jpg'
 import carouselimgsix from '../../assets/img/carouselimgsix.jpg'
+import centralparkone from '../../assets/img/centralparkone.jpg'
+import centralparktwo from '../../assets/img/centralparktwo.jpeg'
+import centralparkthree from '../../assets/img/centralparkthree.jpeg'
+import centralparkfour from '../../assets/img/centralparkfour.jpeg'
+import centralparkfive from '../../assets/img/centralparkfive.jpeg'
+import centralparksix from '../../assets/img/centralparksix.jpeg'
+import centralparkseven from '../../assets/img/centralparkseven.jpeg'
 import logosign from '../../assets/img/logosign.svg'
 import logoomsformen from '../../assets/img/logoomsformen.svg'
 import logodf from '../../assets/img/logodf.svg'
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, EffectCoverflow, Pagination, Navigation } from "swiper";
 
 function About() {
 
-
-    //var counter = 1;
-    //setInterval(function () {
-    // document.getElementById('radio' + counter).checked = true;
-    // counter++;
-    // if (counter > 6) {
-    //counter = 1;
-    // }
-    // }, 6000);
-
+    const swiper2Params = {
+        slidesPerView: "auto",
+        spaceBetween: -30,
+        stretch: 0,
+    };
 
     return (
         <div>
             <div className='first_about_container'>
-                <div className='title_about_container'>
-                    <h1>O Meu Salão</h1>
-                    <p>Fundado em 2019 por Camila Rodrigues, profissional atuante no mercado desde 2003. Sempre buscando por inovações, tecnologias e contribuições diretas com o mercado da beleza.
-                        Vinda de uma tradicional e consagrada família de cabeleireiros e artistas do meio. Sempre com muita paixão pelo aprendizado concebeu o inédito método de mega hair denominado Invisible, técnica esta que obteve destaque em premiações internacionais no ano de 2022 em países da América e Oriente Médio.
-                        <p> Sempre buscando oferecer os melhores serviços de beleza e bem-estar para nossos clientes, com uma sofisticação ímpar, elegância singular
-                            e muito glamour.
-                            Contamos com uma equipe de profissionais altamente qualificadas, que está sempre pronta para atender às suas necessidades e desejos, proporcionando um elegante, sutil e perspicaz ambiente para que você possa relaxar e cuidar de si.
-                        </p>
-                    </p>
-                </div>
+                <h1>O Meu Salão</h1>
+                <p>Fundado em 2019 por Camila Rodrigues, profissional atuante no mercado desde 2003. Sempre buscando por inovações, tecnologias e contribuições diretas com o mercado da beleza. Vinda de uma tradicional e consagrada família de cabeleireiros e artistas do meio. Sempre com muita paixão pelo aprendizado concebeu o inédito método de mega hair denominado Invisible, técnica esta que obteve destaque em premiações internacionais no ano de 2022 em países da América e Oriente Médio.</p>
+                <p>Contamos com uma equipe de profissionais altamente qualificadas, que está sempre pronta para atender às suas necessidades e desejos, proporcionando um elegante, sutil e perspicaz ambiente para que você possa relaxar e cuidar de si.</p>
             </div>
             <div className='second_about_container'>
-                <div className='title_second_container'>
-                    <h1>Conheça as nossas unidades:</h1>
-                </div>
+                <h1>Conheça as nossas unidades:</h1>
             </div>
             <div className='unit_one'>
                 <div className='unit_textblock'>
@@ -74,53 +73,51 @@ function About() {
                 </div>
             </div>
             <div className='container_slider'>
-                <div className='slider'>
-                    <div className='slides'>
-                        <input type="radio" name='radio-btn' id="radio1" />
-                        <input type="radio" name='radio-btn' id="radio2" />
-                        <input type="radio" name='radio-btn' id='radio3' />
-                        <input type="radio" name='radio-btn' id='radio4' />
-                        <input type="radio" name='radio-btn' id='radio5' />
-                        <input type="radio" name='radio-btn' id='radio6' />
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    loop={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                    navigation={{
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                        clickable: true,
+                    }}
+                    modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+                    className='swiper_container2'
 
-                        <div className='slide first'>
-                            <img src={slideimgone} alt='imgone' />
+                    {...swiper2Params}>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={slideimgone} alt="pht_one"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={slideimgtwo} alt="pht_two"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={slideimgthree} alt="pht_third"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={slideimgfour} alt="pht_fourth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={slideimgfive} alt="pht_fifth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={slideimgsix} alt="pht_sixth"></img>
+                    </SwiperSlide>
+                    <div className='slider-controler'>
+                        <div className='swiper-button-prev slider-arrow'>
                         </div>
-                        <div className='slide'>
-                            <img src={slideimgtwo} alt='imgtwo' />
+                        <div className='swiper-button-next slider-arrow'>
                         </div>
-                        <div className='slide'>
-                            <img src={slideimgthree} alt='imgthree' />
-                        </div>
-                        <div className='slide'>
-                            <img src={slideimgfour} alt='imgfour' />
-                        </div>
-                        <div className='slide'>
-                            <img src={slideimgfive} alt='imgfour' />
-                        </div>
-                        <div className='slide'>
-                            <img src={slideimgsix} alt='imgfour' />
-                        </div>
+                        <div className='swiper-pagination'></div>
                     </div>
-
-                    <div className='navigation_auto'>
-                        <div className='auto_btn1'></div>
-                        <div className='auto_btn2'></div>
-                        <div className='auto_btn3'></div>
-                        <div className='auto_btn4'></div>
-                        <div className='auto_btn5'></div>
-                        <div className='auto_btn6'></div>
-                    </div>
-
-                    <div className='manual_navigation'>
-                        <label for="radio1" className='manual-btn' />
-                        <label for="radio2" className='manual-btn' />
-                        <label for="radio3" className='manual-btn' />
-                        <label for="radio4" className='manual-btn' />
-                        <label for="radio5" className='manual-btn' />
-                        <label for="radio6" className='manual-btn' />
-                    </div>
-                </div>
+                </Swiper>
                 <div className='logocontainer'>
                     <img src={logosign} alt='logosign' />
                 </div>
@@ -131,7 +128,6 @@ function About() {
                     <p>Nossa segunda unidade, O meu Salão For Men é uma barbearia, exclusiva para eles.</p>
                     <p>Um espaço super moderno e completo, que possui uma loja de roupas, acessórios e calçados integrada. Disponibilizamos de frigobar e TV, proporcionando a você um conforto maior enquanto você aguarda!</p>
                     <p>Contamos com profissionais super qualificados que entregam um serviço de excelência, possuímos equipamentos de ponta, para garantir uma finalização com grande estilo.</p>
-                    <p>Venha nos fazer uma visita e desfrutar do conforto e qualidade que proporcionamos aos nossos clientes.</p>
                     <div className='hour_phone'>
                         <p><strong>TELEFONE</strong></p>
                         <div className='icon_wpp'>
@@ -149,59 +145,130 @@ function About() {
                     <iframe width="600" height="500" id="gmap_canvas" title='mapcentralpark' src="https://maps.google.com/maps?q=o%20meu%20salao%20for%20men&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
                 </div>
             </div>
-            <div className='container_slidertwo'>
-                <div className='slider'>
-                    <div className='slides'>
-                        <input type="radio" name='radio-btn' id="radio7" />
-                        <input type="radio" name='radio-btn' id="radio8" />
-                        <input type="radio" name='radio-btn' id='radio9' />
-                        <input type="radio" name='radio-btn' id='radio10' />
-                        <input type="radio" name='radio-btn' id='radio11' />
-                        <input type="radio" name='radio-btn' id='radio12' />
+            <div className='container_slider'>
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    loop={true}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    navigation={{
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                        clickable: true,
+                    }}
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                    modules={[Autoplay, EffectCoverflow, Pagination, Navigation]}
+                    className='swiper_container2'
 
-                        <div className='slide first'>
-                            <img src={carouselimgone} alt='carouselimgone' />
+                    {...swiper2Params}>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={carouselimgone} alt="pht_one"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={carouselimgtwo} alt="pht_two"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={carouselimgthree} alt="pht_third"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={carouselimgfour} alt="pht_fourth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={carouselimgfive} alt="pht_fifth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide className='swiper_item'>
+                        <img src={carouselimgsix} alt="pht_sixth"></img>
+                    </SwiperSlide>
+                    <div className='slider-controler'>
+                        <div className='swiper-button-prev slider-arrow'>
                         </div>
-                        <div className='slide'>
-                            <img src={carouselimgtwo} alt='carouselimgtwo' />
+                        <div className='swiper-button-next slider-arrow'>
                         </div>
-                        <div className='slide'>
-                            <img src={carouselimgthree} alt='carouselimgthree' />
-                        </div>
-                        <div className='slide'>
-                            <img src={carouselimgfour} alt='carouselimgfour' />
-                        </div>
-                        <div className='slide'>
-                            <img src={carouselimgfive} alt='carouselimgfour' />
-                        </div>
-                        <div className='slide'>
-                            <img src={carouselimgsix} alt='carouselimgfour' />
-                        </div>
+                        <div className='swiper-pagination'></div>
                     </div>
-
-                    <div className='navigation_auto'>
-                        <div className='auto_btn7'></div>
-                        <div className='auto_btn8'></div>
-                        <div className='auto_btn9'></div>
-                        <div className='auto_btn10'></div>
-                        <div className='auto_btn11'></div>
-                        <div className='auto_btn12'></div>
-                    </div>
-
-                    <div className='manual_navigation'>
-                        <label for="radio7" className='manual-btn' />
-                        <label for="radio8" className='manual-btn' />
-                        <label for="radio9" className='manual-btn' />
-                        <label for="radio10" className='manual-btn' />
-                        <label for="radio11" className='manual-btn' />
-                        <label for="radio12" className='manual-btn' />
-                    </div>
-
-                </div>
+                </Swiper>
                 <div className='logocontainer'>
                     <img src={logoomsformen} alt='logoomsformen' />
                 </div>
             </div>
+            <div className='unit_one'>
+                <div className='unit_textblock'>
+                    <h1>CENTRAL PARK II - BETÂNIA</h1>
+                    <p>Nossa segunda unidade, O meu Salão For Men é uma barbearia, exclusiva para eles.</p>
+                    <p>Um espaço super moderno e completo, que possui uma loja de roupas, acessórios e calçados integrada. Disponibilizamos de frigobar e TV, proporcionando a você um conforto maior enquanto você aguarda!</p>
+                    <p>Contamos com profissionais super qualificados que entregam um serviço de excelência, possuímos equipamentos de ponta, para garantir uma finalização com grande estilo.</p>
+                    <div className='hour_phone'>
+                        <p><strong>TELEFONE</strong></p>
+                        <div className='icon_wpp'>
+                            <div className='wpp'><i className='fab fa-whatsapp'></i></div>
+                            <h1>(31) 9 9828-1212</h1>
+                        </div>
+                        <p><strong>SEGUNDA A SÁBADO</strong></p>
+                        <div className='icon_clock'>
+                            <div className='clock'><i className='fas fa-clock'></i></div>
+                            <h1>09H - 20H</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='map'>
+                    <iframe width="600" height="500" id="gmap_canvas" title='mapcentralpark' src="https://maps.google.com/maps?q=o%20meu%20salao%20for%20men&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
+            </div>
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    loop={true}
+                    slidesPerView="auto"
+                    coverflowEffect={{
+                        rotate: 0,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 2.5,
+                    }}
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                    navigation={{
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                        clickable: true,
+                    }}
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    className='swiper_container'
+
+                >
+                    <SwiperSlide>
+                        <img src={centralparkone} alt="pht_one"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={centralparktwo} alt="pht_two"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={centralparkthree} alt="pht_third"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={centralparkfour} alt="pht_fourth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={centralparkfive} alt="pht_fifth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={centralparksix} alt="pht_fifth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={centralparkseven} alt="pht_fifth"></img>
+                    </SwiperSlide>
+                    <div className='slider-controler'>
+                        <div className='swiper-button-prev slider-arrow'>
+                        </div>
+                        <div className='swiper-button-next slider-arrow'>
+                        </div>
+                        <div className='swiper-pagination'></div>
+                    </div>
+                </Swiper>
             <div className='footer'>
                 <div className='footer_icon'>
                     <a href="https://www.instagram.com/omeusalaoburitis" target="_blank" rel="noopener noreferrer">
--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -5,20 +5,41 @@ import { Link } from 'react-router-dom'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
+import acessibilityicon from '../../assets/img/acessibilityicon.svg'
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { EffectCards, Pagination, Navigation } from "swiper";
+import pht_one from '../../assets/img/pht_one.jpg'
+import pht_two from '../../assets/img/pht_two.jpg'
+import pht_third from '../../assets/img/pht_third.jpg'
+import pht_fourth from '../../assets/img/pht_fourth.jpg'
+import pht_fifth from '../../assets/img/pht_fifth.jpg'
+import pht_sixth from '../../assets/img/pht_sixth.jpg'
+import pht_seventh from '../../assets/img/pht_seventh.jpg'
+import pht_tenth from '../../assets/img/pht_tenth.jpg'
+import pht_ninth from '../../assets/img/pht_ninth.png'
+import acessibilitycardsicon from '../../assets/img/acessibilitycardsicon.svg'
+import logodf from '../../assets/img/logodf.svg'
 
 function Services() {
 
     useEffect(() => {
-        Aos.init({duration: 3000});
-    },[])
+        Aos.init({ duration: 3000 });
+    }, [])
+
 
     return (
         <div>
             <div className='first_container_services'>
-                <div className='title_container_services'>
-                    <h1>Todas as possibilidades reunidas em um só lugar</h1>
-                    <p>Faça parte do nosso clube de vantagens, aproveite os benefícios e tenha acesso a todos os serviços com condições especiais para assinantes.</p>
-                </div>
+                <h1>Todas as possibilidades reunidas em um só lugar</h1>
+                <p>Faça parte do nosso clube de vantagens, aproveite os benefícios e tenha acesso a todos os serviços com condições especiais para assinantes.</p>
+            </div>
+            <div className='acessibility_container'>
+                <h3>Pressione e saiba mais.</h3>
+                <img src={acessibilityicon} alt='slide' />
             </div>
             <div className='benefitspackservices'>
                 <div className='benefits_pack'>
@@ -51,45 +72,57 @@ function Services() {
                     </div>
                 </div>
             </div>
+            <div className='acessibility_container'>
+                <img src={acessibilitycardsicon} alt='acessibilitycardsicon' />
+            </div>
             <div className='second_container_services'>
-                <div className='listservices_box'>
-                    <div className='item_listservices' data-aos="fade-right">
+                <Swiper
+                    effect={'cards'}
+                    grabCursor={true}
+                    loop={true}
+                    centeredSlides={true}
+                    slidesPerView="auto"
+                    modules={[EffectCards, Pagination, Navigation]}
+                    className='swiper_container'
+
+                >
+                    <SwiperSlide>
                         <h1>Sobrancelha</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
+                        <img src={pht_one} alt="pht_one"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Micropigmentação Labial</h1>
+                        <img src={pht_two} alt="pht_two"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
                         <h1>Esmalteria</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Baby liss</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Escova</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Corte</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Hidratação</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
+                        <img src={pht_third} alt="pht_third"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Penteados</h1>
+                        <img src={pht_fourth} alt="pht_fourth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Makeup</h1>
+                        <img src={pht_fifth} alt="pht_fifth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
                         <h1>Massagem</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Spa dos Pés</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Selagem</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Botox</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Podologia</h1>
-                    </div>
-                    <div className='item_listservices' data-aos="fade-right">
-                        <h1>Drenagem</h1>
-                    </div>
-                </div>
+                        <img src={pht_sixth} alt="pht_sixth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Dream Gloss</h1>
+                        <img src={pht_seventh} alt="pht_seventh"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Mechas</h1>
+                        <img src={pht_tenth} alt="pht_tenth"></img>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Cílios</h1>
+                        <img src={pht_ninth} alt="pht_ninth"></img>
+                    </SwiperSlide>
+                </Swiper>
             </div>
             <div className='buttonscheduling'>
                 <Link to='/Contact'>
@@ -102,12 +135,10 @@ function Services() {
                         <i className='fab fa-instagram'></i>
                     </a>
                 </div>
-                <div className='footer_logo'>
-                    <img onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }); }} src={Oslogo} alt='Oslogo' />
-                </div>
-                <div className='footer_icon'>
-                    <a href="https://wa.me/5531991021509?text=Ol%C3%A1%2C+venho+do+Site+O+Meu+Salao+Buritis" target="_blank" rel="noopener noreferrer">
-                        <i className='fab fa-whatsapp'></i>
+                <div className='auth_prod'>
+                    <h1>Powered by</h1>
+                    <a href="https://www.designfactoryweb.com.br" target="_blank" rel="noopener noreferrer">
+                        <img src={logodf} alt="logodf"></img>
                     </a>
                 </div>
             </div>
